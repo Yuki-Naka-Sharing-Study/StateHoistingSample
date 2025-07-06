295472793d6954a22185f929c8839a906ad06885
仲優樹 <yu-ki216m@outlook.jp>
2025-07-06 12:40:21 +0900

    Created ClientFormViewModel.

--- app/src/main/java/com/example/statehoistingsample/viewmodel/ClientFormViewModel.kt
+++ app/src/main/java/com/example/statehoistingsample/viewmodel/ClientFormViewModel.kt
@@ -1,2 +1,28 @@
 package com.example.statehoistingsample.viewmodel
 
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import com.example.statehoistingsample.ClientType
+
+class ClientFormViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(ClientFormUiState())
+    val uiState: StateFlow<ClientFormUiState> = _uiState
+
+    fun onClientTypeChange(type: ClientType) {
+        _uiState.value = _uiState.value.copy(clientType = type)
+    }
+
+    fun onNameChange(name: String) {
+        _uiState.value = _uiState.value.copy(name = name)
+    }
+
+    fun onCompanyNameChange(companyName: String) {
+        _uiState.value = _uiState.value.copy(companyName = companyName)
+    }
+
+    fun onContactNameChange(contactName: String) {
+        _uiState.value = _uiState.value.copy(contactName = contactName)
+    }
+}
